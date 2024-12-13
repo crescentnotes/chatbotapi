680524c74bc1ce21ef024b679471829efc7829eb
irfan mohamed <mohamedirfank700@gmail.com>
2024-12-14 00:52:43 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app=express();
+const port =3000;
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+app.post('/chatbot',(req,res)=>{
+  res.send("helo from chat bot")
+
+
+})
+
+app.listen(port,()=>{
+    console.log(`server is listening on port ${port}`);
+});
